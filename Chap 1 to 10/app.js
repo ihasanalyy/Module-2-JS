19980d769a1acb85bb56712ca744afa51d4b8bc5
Hassan Ali <139680294+ihasanalyy@users.noreply.github.com>
2024-03-09 11:59:49 +0500

    Add files via upload

---
+++ Chap 1 to 10/app.js
@@ -0,0 +1,102 @@
+// ALerts/////////////////////////
+
+// alert("Error! Please enter a valid password.")
+
+// alert("Welcome to JS Land. \n Happy Coding")
+
+// alert("Hello I can run JS through my web browser console")
+// console.log(alert)
+
+// Variable for Strings/////////////////////////
+
+// var x = "user.name"
+// console.log(x)
+
+// var myname = "Hassan Ali"
+// console.log(myname)
+
+// var message = "Hello World"
+// alert(message)
+
+// var username = "Hassan"
+// var age = "24 years old"
+// var course = "Certified Web dev"
+// alert(username)
+// alert(age)
+// alert(course)
+
+// var message = "PIZZA \n PIZZ \n PIZ \n PI \n P"
+// alert(message) 
+
+
+// var message = "my email address is "
+// var emailaddress ="example@example.com"
+// alert(message+emailaddress)
+
+
+// var book ="“A smarter way to learn JavaScript”."
+// alert(book)
+
+// var x = "▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬"
+// alert(x)
+
+// Variable for Numbers
+
+// var age = 10
+// var x = " years old"
+// alert(age+x)
+
+// var message = "You have visited "
+// x = 14
+// end = " times"
+// alert(message+x+end)
+
+// var firstName = "Hassan",age= 15, lastName="Ali"
+// alert(firstName+lastName+age)
+
+// 5 legal & 5 illegal
+
+// my Variable 4myVariable myVariable!
+// my_variable my$variable
+
+// MATH EXPRESSIONS
+
+// var x = 10
+// var y = 10
+// console.log(x+y)
+
+// var x = 10
+// var y = 5
+// console.log(x-y)
+
+// var x = 10
+// var y = 5
+// console.log(x/y)
+
+// var x = 10
+// var y = 5
+// console.log(x*y)
+
+// var ticket_price = 600
+//     ticket_qty = 5
+//     console.log(ticket_price*ticket_qty)
+
+// var total = "Total cost of your order is "
+//    price_of_item1 = 650
+//    price_of_item2 = 100
+//    Ordered_quantity_of_item1 = 3
+//    Ordered_quantity_of_item2 = 7
+//    Shipping_charges = 100
+// console.log(total+price_of_item1*Ordered_quantity_of_item1+price_of_item2*Ordered_quantity_of_item2+Shipping_charges)
+
+
+// var a = 2, b = 1
+// console.log (--a - --b + ++b + b--)
+// Pre decrement
+//  console.log(--a)
+// Pre decrement in both
+//  console.log(--a - --b)
+// Pre decrement in 1st and 2nd/// Pre increment in last
+// console.log(--a - --b + ++b)
+// Pre decrement in 1st and 2nd/// Pre increment in 3rd // Post decrement in last
+// console.log(--a - --b + ++b + b--)
