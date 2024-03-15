e44f95c0477783144da484c117be320a41daa145
Hassan Ali <139680294+ihasanalyy@users.noreply.github.com>
2024-03-16 03:01:02 +0500

    Add files via upload

---
+++ Chap 12 to 16/app.js
@@ -0,0 +1,106 @@
+// var userInput = prompt("Enter character or number")
+
+// if (userInput >= "A" && userInput <= "Z" ){
+//     console.log("This is an UPPER case letter")
+// } else if (userInput >= "a" && userInput <= "z" ){
+//     console.log("This is an lower case letter")
+// } else if (userInput >= 1) {
+//     console.log("This is number")
+// } else {
+//     console.log("Invalid input")
+// }
+
+// var num1 = +prompt("enter num 1")
+//     num2 = +prompt("enter num 2")
+
+// if (num1 > num2) {
+//     console.log(num1+ "is larger")
+// } else if (num1 < num2) {
+//     console.log(num2+ "is larger")
+// } else if (num1 === num2) {
+//     console.log("both numbers are equals")
+// } else {
+//     console.log("Error")
+// }
+
+// var userInput = +prompt("enter any number")
+
+// if (userInput >= 1) {
+//     console.log(userInput + "this number is positive")
+// } else if (userInput <= -1 ){
+//     console.log(userInput + "this number is negative")
+// }else if (userInput === 0){
+//     console.log("this number is zero")
+// } else {
+//     console.log("error")
+// }
+
+// var userInput = prompt("Enter any character")
+
+// if (userInput === "A" || userInput === "E" || userInput === "I" || userInput === "O" || userInput === "U"){
+//     console.log("THIS IS VOWEL")
+// } else {
+//     console.log("THIS IS consonants")
+// }
+
+
+// var userPassword = prompt("Enter your password")
+//     DBpassword = "hello"
+
+// if (userPassword === DBpassword) {
+//     console.log("welcome")
+// } else {
+//     console.log("incorrect password")
+// }
+
+
+// var greeting;
+//  var hour = 13;
+//   if (hour < 18) { greeting = "Good day";
+//     else greeting = "Good evening"; }
+
+
+// var time = +prompt("Enter time")
+
+// if (time >=0 || time < 6){
+//     console.log("Good night")
+// } else if (time >=6 || time < 12){
+//     console.log("Good morning")
+// } else if (time >=12 || time < 16){
+//     console.log("Good afternoon")
+// } else if (time >=16 || time <= 24){
+//     console.log("Good evening")
+// }
+
+
+// var studentArr = []
+
+
+// var strings = ["strings1","strings2","strings3","strings4"]
+
+// console.log(strings)
+
+
+// var num = [1,2,3,4,5,6]
+
+// console.log(num)
+
+
+// var boolean = [true,false,false,true,true]
+
+// console.log(boolean)
+
+
+// var mixed = [true,false,32,44,"cat"]
+
+// console.log(mixed)
+
+
+// var qualifications = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"]
+
+// console.log(qualifications)
+
+
+// var studentsArr = ["Pawn","Shawn","Michael"]
+//     scoreArr = [300,400,350]
+//     console.log(studentsArr + scoreArr)
